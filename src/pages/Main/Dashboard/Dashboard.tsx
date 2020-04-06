c4a3a47da805f80bfda8cbb2291b575c6c9559f6
StanisalvkoP <stas8327634@gmail.com>
2020-04-06 12:49:23 +0300

    Added dashboard container

---
+++ src/pages/Main/Dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { ProjectListWithSearchContainer } from "containers/ProjectListWithSearchContainer/ProjectListWithSearchContainer";
+
+export class Dashboard extends React.Component {
+
+
+    render() {
+        return (
+            <>
+                <ProjectListWithSearchContainer />
+            </>
+        )
+    }
+}
