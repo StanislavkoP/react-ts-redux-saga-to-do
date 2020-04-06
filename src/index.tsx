5a469a82418fba90abf73f1ada494919be92f312
StanisalvkoP <stas8327634@gmail.com>
2020-04-06 12:40:43 +0300

    Added firebase initializing and subscribing to auth changing

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,41 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import store from './redux/store';
-import MainRouter from './router';
+import firebase from "firebase/app";
+import "firebase/auth";
+import store from 'redux/store';
+import MainRouter from 'router';
 
 import './index.scss';
+import {AuthActions} from "./redux/auth/actions";
+import {IAuth, IUser} from "./types";
+import {UserActions} from "./redux/user/actions";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCqqYzOok93vKGl5PqWnv-JqLXSNYHszvg",
+    authDomain: "react-ts-to-do.firebaseapp.com",
+    databaseURL: "https://react-ts-to-do.firebaseio.com",
+    projectId: "react-ts-to-do",
+    storageBucket: "react-ts-to-do.appspot.com",
+    messagingSenderId: "168333316025",
+    appId: "1:168333316025:web:bf4b4a2b6eaa4882f63068"
+};
+firebase.initializeApp(firebaseConfig);
+
+firebase.auth().onAuthStateChanged(async user => {
+   if (user) {
+       const userData: IUser= {
+           email: user.email
+       };
+       const authData: IAuth = {
+           refreshToken: user.refreshToken
+       };
+
+       store.dispatch(AuthActions.authSuccess(authData));
+       store.dispatch(UserActions.setUser(userData));
+   }
+});
 
 const app = (
     <Provider store={store}>
