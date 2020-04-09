e5bd07101e884754cd0bb28eeed80314d11e8fd0
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:08:47 +0300

    Updated root reducer

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,29 +1,30 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { combineReducers } from 'redux';
-import { useSelector, TypedUseSelectorHook } from 'react-redux'
-import { routerReducer } from 'react-router-redux';
-import { connectRouter, RouterState } from 'connected-react-router'
-import { History } from 'history'
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
+import { connectRouter, RouterState } from 'connected-react-router';
+import { History } from 'history';
 import { authReducer } from './auth/reducer';
-import { IUser, IAuth } from "../types";
+import { IUser } from "types/user";
+import { IAuth } from "types/auth";
 import { userReducer } from "./user/reducer";
 import { projectsReducer, IProjectsReducer } from "./projects/reducer";
+import { IUsersReducer, usersReducer } from "./users/reducer";
 
 export interface IRootReducer {
-    router: RouterState,
-    authReducer: IAuth,
-    userReducer: IUser,
-    projectsReducer: IProjectsReducer
+    router: RouterState;
+    authReducer: IAuth;
+    userReducer: IUser;
+    usersReducer: IUsersReducer;
+    projectsReducer: IProjectsReducer;
 }
 
 export const useTypedSelector: TypedUseSelectorHook<IRootReducer> = useSelector;
 
-// NOTE: current type definition of Reducer in 'redux-actions' module
-// doesn't go well with redux@4
 const rootReducer = (history: History) => combineReducers<IRootReducer>({
     router: connectRouter(history),
     authReducer: authReducer as any,
     userReducer: userReducer as any,
+    usersReducer: usersReducer as any,
     projectsReducer: projectsReducer as any
 });
 
