17ff6d7bcd16b175a8be5e5f2e353c5f828f8ba7
StanisalvkoP <stas8327634@gmail.com>
2020-04-10 01:27:22 +0300

    Added empty icon

--- src/containers/Statistic/ProjectsStatisticListContainer/ProjectsStatisticListContainer.tsx
+++ src/containers/Statistic/ProjectsStatisticListContainer/ProjectsStatisticListContainer.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import { ProjectStatisticCardList } from "components/Statistic/ProjectStatisticCardList/ProjectStatisticCardList";
 import { useTypedSelector } from "redux/rootReducer";
+import { Empty } from "antd";
 
 export function ProjectsStatisticListContainer() {
     const projects = useTypedSelector(state => state.projectsReducer.projects);
 
+    if (!projects.ids.length) {
+        return (
+            <Empty
+                image={Empty.PRESENTED_IMAGE_SIMPLE}
+                description='No projects'
+            />
+        );
+    }
 
     return (
         <ProjectStatisticCardList ids={projects.ids} list={projects.list}/>
