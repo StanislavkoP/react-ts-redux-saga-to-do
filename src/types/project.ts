0f2db084dc0516e284681a1b08cb2484b113e6b3
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:14:37 +0300

    Updated project type

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,11 +1,31 @@
-export interface ITask {
-    text: string;
-    assigned: number;
-}
+import { IAssignedUser } from "./user";
 
 export interface IProject {
     id: string;
     title: string;
     description: string;
-    tasks: Array<ITask>;
+    tasksCount: number;
+    completedCount: number;
+    tasks: {
+        [key: string]: ITask
+    };
+}
+
+export interface IProjectForm {
+    title: IProject['title'];
+    description: IProject['description'];
+}
+
+
+export interface ITask {
+    id: string;
+    description: string;
+    assigned?: IAssignedUser | null;
+    completed: boolean;
+}
+
+export interface ITaskForm {
+    description: ITask['description'];
+    assigned: IAssignedUser['id'] | null;
+    completed: ITask['completed'];
 }
