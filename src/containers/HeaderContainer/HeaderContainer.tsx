5560b49078fa244d238474ee6154e4d06a476428
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:00:21 +0300

    Hidden header if user is not logged in

--- src/containers/HeaderContainer/HeaderContainer.tsx
+++ src/containers/HeaderContainer/HeaderContainer.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useTypedSelector } from "redux/rootReducer";
 import { Header } from "components/Header/Header";
 
 export function HeaderContainer() {
+    const currentUser = useTypedSelector(state => state.userReducer);
+    const auth = useTypedSelector(state => state.authReducer);
     const location = useLocation();
 
+    if (!auth) return null;
+
     return (
-        <Header path={location.pathname}/>
+        <Header path={location.pathname} userName={currentUser && currentUser.email} />
     );
 }
