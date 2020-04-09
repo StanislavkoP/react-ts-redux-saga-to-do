58765f32fc0d43e9ddf4d90e1d67a038dc75d72b
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 21:54:04 +0300

    Added form for adding a project

---
+++ src/components/Projects/AddProjectForm/AddProjectForm.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { FormikErrors } from "formik";
+import { Button, Col, Form, Input, Row } from "antd";
+import { IProjectForm } from "types/project";
+import { Textarea } from "components/common/Textarea/Textarea";
+
+const layout = {
+    labelCol: { span: 6 },
+    wrapperCol: { span: 18 },
+};
+
+interface IAddProjectForm {
+    isSubmitting: boolean;
+    values: IProjectForm;
+    errors: FormikErrors<IProjectForm>;
+    onSubmit: () => void;
+    onChangeField: (e: React.ChangeEvent) => void;
+    onCancel: () => void;
+}
+
+export function AddProjectForm({
+    errors,
+    values,
+    isSubmitting,
+    onSubmit,
+    onCancel,
+    onChangeField,
+}: IAddProjectForm) {
+    return (
+        <Form {...layout}>
+            <Form.Item
+                label="Title"
+                wrapperCol={{ ...layout.wrapperCol }}
+                validateStatus={errors.title ? "error" : undefined}
+                help={errors.title}
+            >
+                <Input
+                    name="title"
+                    value={values.title}
+                    onChange={onChangeField}
+                    placeholder='Project title'
+                />
+            </Form.Item>
+            <Form.Item
+                label="Description"
+                wrapperCol={{ ...layout.wrapperCol }}
+                validateStatus={errors.description ? "error" : undefined}
+                help={errors.description}
+
+            >
+                <Textarea
+                    name="description"
+                    value={values.description}
+                    onChange={onChangeField}
+                    placeholder='Project description'
+                    autoSize={{ minRows: 3, maxRows: 5 }}
+                />
+            </Form.Item>
+            <Row justify="space-between">
+                <Col span={11}>
+                    <Button type='primary' block onClick={onSubmit} loading={isSubmitting}>Add</Button>
+                </Col>
+                <Col span={11}>
+                    <Button  type='primary' block danger onClick={onCancel}>Cancel</Button>
+                </Col>
+            </Row>
+        </Form>
+
+    );
+}
---
+++ src/containers/Projects/AddProjectFormContainer/AddProjectFormContainer.tsx
@@ -0,0 +1,76 @@
+import React, { useContext } from 'react';
+import { useDispatch } from "react-redux";
+import { useFormik } from 'formik';
+import { IProjectForm } from "types/project";
+import { ProjectsActions } from "redux/projects/actions";
+import { projectValidationSchema } from "validationSchemas";
+import { ProjectContextApi } from "contexts/projectContextApi";
+import { message } from "antd";
+import { AddProjectForm } from "components/Projects/AddProjectForm/AddProjectForm";
+
+interface IAddProjectFormContainer {
+    onOpenModal: () => void;
+}
+
+
+export function AddProjectFormContainer({ onOpenModal }: IAddProjectFormContainer) {
+    const dispatch = useDispatch();
+    const formik = useFormik<IProjectForm>({
+        initialValues: {
+            title: '',
+            description: '',
+        },
+        validationSchema: projectValidationSchema,
+        onSubmit: () => {}
+    });
+    const useProjectApiCtx = useContext(ProjectContextApi);
+
+
+
+    async function onCreateProject() {
+        const values = formik.values;
+        const errors = await formik.validateForm();
+        if (Object.keys(errors).length) {
+            return;
+        }
+
+        formik.setSubmitting(true);
+
+        const newProjectData = {
+            ...values,
+            completedCount: 0,
+            tasksCount: 0,
+            tasks: {},
+        };
+
+        useProjectApiCtx?.createProject(newProjectData)
+            .then((project) => {
+                dispatch(ProjectsActions.createProject(project));
+
+                message.success('You added a new project successfully');
+                formik.resetForm();
+                onOpenModal();
+            })
+            .catch((error) => {
+                console.log(error);
+                formik.setSubmitting(false);
+            });
+    }
+
+    function onCancelForm() {
+        formik.resetForm();
+        onOpenModal();
+    }
+
+
+    return (
+        <AddProjectForm
+            errors={formik.errors}
+            values={formik.values}
+            isSubmitting={formik.isSubmitting}
+            onSubmit={onCreateProject}
+            onCancel={onCancelForm}
+            onChangeField={formik.handleChange}
+        />
+    );
+}
