82f07923a74b5badf81356e1e5d64fa1b5ce804e
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:18:07 +0300

    Refactored auth s

--- src/redux/auth/actions/index.ts
+++ src/redux/auth/actions/index.ts
@@ -1,17 +1,19 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 import { createAction } from 'redux-actions';
-import { IUser } from 'types';
 
 enum Type {
-    AUTH_SUCCESS = 'AUTH_SUCCESS',
+    AUTH_SUCCESS = 'auth/authSuccess',
+    LOG_OUT = 'auth/logOut',
 }
 
-const authSuccess = createAction(Type.AUTH_SUCCESS, );
+const authSuccess = createAction(Type.AUTH_SUCCESS);
+const logOut = createAction(Type.LOG_OUT);
 
 export const AuthActions = {
     Type,
 
-    authSuccess
+    authSuccess,
+    logOut
 };
 
 export type AuthActions = Omit<typeof AuthActions, 'Type'>;
--- src/redux/auth/sagas/index.ts
+++ src/redux/auth/sagas/index.ts
@@ -1,13 +1,29 @@
 import { push } from 'react-router-redux';
-import { put, takeEvery } from 'redux-saga/effects'
-import {AuthActions} from "../actions";
+import { put, takeEvery } from 'redux-saga/effects';
+import { AuthActions } from "../actions";
+import { ProjectsActions } from "../../projects/actions";
+import { UsersActions } from "../../users/actions";
+import { UserActions } from "../../user/actions";
 
-function* changeRoute() {
-    yield put(push('/projects'))
+function* changeRoute(route: string) {
+    yield put(push(route));
+}
+
+function* authSuccess() {
+    yield changeRoute( '/');
+    yield put(ProjectsActions.getProjects());
+    yield put(UsersActions.getUsers());
+}
+
+function* logOut() {
+    yield put(UserActions.clearUser());
+    yield put(UsersActions.clearUsers());
+    yield put(ProjectsActions.clearProjects());
 }
 
 export function* watchUserAuthed() {
-    yield takeEvery(AuthActions.Type.AUTH_SUCCESS, changeRoute)
+    yield takeEvery(AuthActions.Type.AUTH_SUCCESS, authSuccess);
+    yield takeEvery(AuthActions.Type.LOG_OUT, logOut);
 }
 
 export const watchersAuthSagas = [watchUserAuthed()];
