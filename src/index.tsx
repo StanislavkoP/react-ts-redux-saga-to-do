986a340afcc334dfd6a676579458c3f73e9816f1
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:19:26 +0300

    Refactored

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
+import * as serviceWorker from './serviceWorker';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import * as serviceWorker from './serviceWorker';
 import firebase from "firebase/app";
 import "firebase/auth";
 import store from 'redux/store';
+import { IUser } from "types/user";
+import { IAuth } from "types/auth";
+import { AuthActions } from "redux/auth/actions";
+import { UserActions } from "redux/user/actions";
+import { firebaseConfig } from "config/firebase";
 import MainRouter from 'router';
 
 import './index.scss';
-import {AuthActions} from "./redux/auth/actions";
-import {IAuth, IUser} from "./types";
-import {UserActions} from "./redux/user/actions";
 
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCqqYzOok93vKGl5PqWnv-JqLXSNYHszvg",
-    authDomain: "react-ts-to-do.firebaseapp.com",
-    databaseURL: "https://react-ts-to-do.firebaseio.com",
-    projectId: "react-ts-to-do",
-    storageBucket: "react-ts-to-do.appspot.com",
-    messagingSenderId: "168333316025",
-    appId: "1:168333316025:web:bf4b4a2b6eaa4882f63068"
-};
 firebase.initializeApp(firebaseConfig);
 
 firebase.auth().onAuthStateChanged(async user => {
