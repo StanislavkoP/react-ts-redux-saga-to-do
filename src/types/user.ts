6f586c8103a3686aeb4605a0700f9bf63cceb9eb
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 21:49:24 +0300

    Updated user types

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,7 +1,8 @@
-export interface IAuth {
-    refreshToken: string;
-}
-
 export interface IUser {
     email: string | null;
+}
+
+export interface IAssignedUser {
+    id: string;
+    email: string;
 }
