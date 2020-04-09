8574ca643d37a681f606b8c534b80b90ec5bc8cc
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:09:00 +0300

    Updated root saga

--- src/redux/rootSagas.ts
+++ src/redux/rootSagas.ts
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects';
 import { watchersAuthSagas } from "./auth/sagas";
 import { watchersGetProjects } from "./projects/sagas";
+import { watchersUsers } from "./users/sagas";
 
 export default function* rootSaga() {
     yield all([
         ...watchersAuthSagas,
         ...watchersGetProjects,
+        ...watchersUsers
     ]);
 }
