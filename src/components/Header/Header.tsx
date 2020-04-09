2f18316bc7228d8880d8aa8916828d5202738924
StanisalvkoP <stas8327634@gmail.com>
2020-04-09 22:00:43 +0300

    Updated design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Menu } from 'antd';
+import { Col, Layout, Menu, Row } from 'antd';
+
+const { Header: AntHeader } = Layout;
 
 interface IHeader {
-    path: string
+    path: string;
+    userName: string | null | undefined;
 }
 
-export function Header({ path }: IHeader) {
+export function Header({ path, userName }: IHeader) {
     return (
-        <Menu selectedKeys={[path]} mode="horizontal">
-            <Menu.Item key="logout">
-                <Link to="/logout">
-                    Log Out
-                </Link>
-            </Menu.Item>
-        </Menu>
+        <AntHeader className="main-header">
+            <Row justify="center">
+                <Col xs={24} sm={24} lg={16}>
+                    <Row justify="space-between">
+                        <Col xs={18} sm={12}>
+                            <div className="main-header__user">
+                                Hello { userName || null }
+                            </div>
+
+                        </Col>
+
+                        <Col xs={1} sm={12}>
+                            <Menu className="main-header__menu" selectedKeys={[path]} mode="horizontal">
+                                <Menu.Item key="/">
+                                    <Link to="/">
+                                        Projects
+                                    </Link>
+                                </Menu.Item>
+                                <Menu.Item key="/statistic">
+                                    <Link to="/statistic">
+                                        Statistic
+                                    </Link>
+                                </Menu.Item>
+                                <Menu.Item key="/logout">
+                                    <Link to="/logout">
+                                        Log Out
+                                    </Link>
+                                </Menu.Item>
+                            </Menu>
+
+                        </Col>
+                    </Row>
+
+                </Col>
+            </Row>
+
+        </AntHeader>
     );
 }
